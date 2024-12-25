88efccece6c106a11f10e06d87b35f95259e7874
Mohamed Alattar <111948799+MohamedNabil114@users.noreply.github.com>
2024-12-25 22:54:52 +0200

    Initial commit with Heap-Sort and Kruskal's Algorithm implementation
    Added implementation of:
    - Heap-Sort Algorithm with its project file.
    - Kruskal's Algorithm with its project file.
    Included the solution file (Algorithm's task.sln)

---
+++ Heap-Sort Algorithm/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class HeapSort
+{
+    public void Sort(int[] array)
+    {
+        int n = array.Length;
+
+        
+        for (int i = n / 2 - 1; i >= 0; i--)
+            Heapify(array, n, i);
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            Heapify(array, i, 0);
+        }
+    }
+
+    void Heapify(int[] array, int n, int i)
+    {
+        int largest = i; 
+        int left = 2 * i + 1; 
+        int right = 2 * i + 2; 
+
+        if (left < n && array[left] > array[largest])
+            largest = left;
+
+        if (right < n && array[right] > array[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            Heapify(array, n, largest);
+        }
+    }
+
+    public void PrintArray(int[] array)
+    {
+        foreach (int item in array)
+            Console.Write(item + " ");
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 12, 11, 13, 5, 6, 7 };
+        Console.WriteLine("Original Array:");
+        Console.WriteLine(string.Join(" ", array));
+
+        HeapSort heapSort = new HeapSort();
+        heapSort.Sort(array);
+
+        Console.WriteLine("Sorted Array:");
+        heapSort.PrintArray(array);
+        Console.ReadKey();
+    }
+}
+
---
+++ Kruskal’s Algorithm/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Edge : IComparable<Edge>
+{
+    public int Source, Destination, Weight;
+
+    public int CompareTo(Edge compareEdge)
+    {
+        return this.Weight.CompareTo(compareEdge.Weight);
+    }
+}
+
+class KruskalAlgorithm
+{
+    int vertices;
+    List<Edge> edges = new List<Edge>();
+
+    public KruskalAlgorithm(int vertices)
+    {
+        this.vertices = vertices;
+    }
+
+    public void AddEdge(int source, int destination, int weight)
+    {
+        edges.Add(new Edge() { Source = source, Destination = destination, Weight = weight });
+    }
+
+    public void FindMST()
+    {
+        edges.Sort();
+
+        int[] parent = new int[vertices];
+        for (int i = 0; i < vertices; i++)
+            parent[i] = i;
+
+        List<Edge> mst = new List<Edge>();
+
+        foreach (Edge edge in edges)
+        {
+            int root1 = Find(parent, edge.Source);
+            int root2 = Find(parent, edge.Destination);
+
+            if (root1 != root2)
+            {
+                mst.Add(edge);
+                Union(parent, root1, root2);
+            }
+        }
+
+        Console.WriteLine("Edges in MST:");
+        foreach (Edge edge in mst)
+            Console.WriteLine($"{edge.Source} - {edge.Destination} : {edge.Weight}");
+    }
+
+    int Find(int[] parent, int vertex)
+    {
+        if (parent[vertex] != vertex)
+            parent[vertex] = Find(parent, parent[vertex]);
+        return parent[vertex];
+    }
+
+    void Union(int[] parent, int root1, int root2)
+    {
+        parent[root2] = root1;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KruskalAlgorithm graph = new KruskalAlgorithm(4);
+        graph.AddEdge(0, 1, 10);
+        graph.AddEdge(0, 2, 6);
+        graph.AddEdge(0, 3, 5);
+        graph.AddEdge(1, 3, 15);
+        graph.AddEdge(2, 3, 4);
+
+        graph.FindMST();
+        Console.ReadKey();
+    }
+}
+
